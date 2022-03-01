46903931b68b3ffc01aa371dbec4c4d5bf417397
Florencia Pak <florencia.pak@gmail.com>
2022-03-01 16:05:12 -0300

    Correcion de ruta a archivo JS

--- js/scripts.js
+++ js/scripts.js
@@ -53,7 +53,6 @@ window.addEventListener('DOMContentLoaded', event => {
 
     // Activate SimpleLightbox plugin for portfolio items
     new SimpleLightbox({
-        elements: '#portfolio a.portfolio-box'
+        elements: 'a.portfolio-box'
     });
-
 });
